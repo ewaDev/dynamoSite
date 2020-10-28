692698ab9639efd2d225da94515b5f59ee8a19c7
Ewa G <e.grabowiecka@outlook.com>
2020-10-28 22:16:05 +0000

    added a template for a new component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
+import NavigationBar from "../components/navigation-bar/NavigationBar"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return  <NavigationBar/>
 }
