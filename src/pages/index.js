86ee06e603c484a59e05d7d800b8a573ebb72b88
Ewa G <e.grabowiecka@outlook.com>
2020-12-03 19:13:37 +0000

    fixing responsive images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,21 @@
 import React from "react"
-import NavigationBar from "../components/navigation-bar/NavigationBar"
+import styleSheet from "./index.module.css"
 
-export default function Home() {
-  return  <NavigationBar/>
+
+
+const Home = () => {
+  return (
+    <div class={styleSheet.wrapper}>
+    <div class={styleSheet.topContainer}>
+      <div class={styleSheet.topContainerImage}> 
+        <img alt="Dynamo Site Logo" src="../../static/logo.png" class={styleSheet.image}/>
+      </div>
+      <div class={styleSheet.topContainerText}> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
+    </div>
+
+  </div>
+  )
 }
+
+
+export default Home
