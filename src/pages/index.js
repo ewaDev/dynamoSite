6c1918370e99b8bb241501e2c5b38e6523457a21
Ewa G <e.grabowiecka@outlook.com>
2021-01-02 21:41:30 +0000

    fixing logo image

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const Home = () => {
     <div class={styleSheet.wrapper}>
       <div class={styleSheet.topContainer}>
         <div class={styleSheet.topContainerImage}> 
-          <img alt="Dynamo Site Logo" src="/static/logo.png" class={styleSheet.image}/>
+          <img alt="Dynamo Site Logo" src="/logo.png" class={styleSheet.image}/>
         </div>
         <div class={styleSheet.topContainerItems}>
           <div><NavigationBar/></div>
@@ -18,7 +18,6 @@ const Home = () => {
           <div class={styleSheet.solidLine}></div>
           <div class={styleSheet.topContainerText}> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
           </div>
-          {/* <div class={styleSheet.solidLine}></div> */}
         </div>
       </div>
     </div>
